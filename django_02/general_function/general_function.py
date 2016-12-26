b830749482457ce938d6634bfda077141dfc0ef0
ivangolubykh <ivan9@allworld.xyz>
2016-12-26 12:08:09 +0300

    Подправил функцию редиректа на предыдущую страницу. Исправлена ошибка с зацикливанием редиректа страницы на саму себя.

--- django_02/general_function/general_function.py
+++ django_02/general_function/general_function.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, HttpResponseRedirect
+#from django.core.urlresolvers import reverse
+#from django.http import HttpResponse
 
 
 def Return_to_back(request):
     if request.META['HTTP_REFERER']:
-        return HttpResponseRedirect(request.META['HTTP_REFERER'])
+        # if request.path == reverse('login') or request.path == reverse('login'):
+        if request.META['HTTP_REFERER'] == request.build_absolute_uri():
+            return HttpResponseRedirect("/")
+        else:
+            return HttpResponseRedirect(request.META['HTTP_REFERER'])
     else:
         return HttpResponseRedirect("/")
+
