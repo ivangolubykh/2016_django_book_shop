cd48154c2d7d5ba07dd1ab12450867d160e2013b
ivangolubykh <ivan9@allworld.xyz>
2017-03-26 11:35:48 +0300

    1) Срок жизни сессиий увеличен с 2 недель до 5 лет. 2) Проверка на авторизацию админа вынесена в декоратор для большей удобочитаемости кода.

--- django_02/admin_users/views.py
+++ django_02/admin_users/views.py
@@ -23,6 +23,24 @@ from admin_users.forms import Edit_User_Form, Edit_User_Passw
 ITEMS_TO_PAGE = 5  # количество элементов на странице
 
 
+def is_admin(func_to_decorate):
+    '''Декоратор для проверки, является ли посетитель админом.'''
+    def decor(*args, **kwargs):
+        if args[0].user.is_authenticated:
+            if args[0].user.is_staff or args[0].user.is_superuser:
+                return func_to_decorate(*args, **kwargs)
+            else:
+                return HttpResponse(
+                    'Ошибка: У вас нет права администрировать.',
+                    content_type='text/html; charset=utf-8')
+        else:
+            return HttpResponse('Ошибка: Вы не авторизованы',
+                                content_type='text/html; charset=utf-8',
+                                charset='utf-8')
+    return decor
+
+
+# @is_admin
 def Admin_Main(request):
     list = User.objects.order_by('username')
     paginator = Paginator(list, ITEMS_TO_PAGE)
@@ -30,160 +48,147 @@ def Admin_Main(request):
                   {'list': paginator.page(1)})
 
 
-def Veryfy_Admin_Autorizations(request):
-    if request.user.is_authenticated:
-        if request.user.is_staff or request.user.is_superuser:
-            return True
-        else:
-            return HttpResponse('Ошибка: У вас нет права администрировать.',
-                                content_type='text/html; charset=utf-8')
-    else:
-        return HttpResponse('Ошибка: Вы не авторизованы',
-                            content_type='text/html; charset=utf-8',
-                            charset='utf-8')
-
-
+@is_admin
 def Admin_Change_Data(request):
-    if Veryfy_Admin_Autorizations(request):
-        if request.method == "POST" and request.is_ajax:
+    if request.method == "POST" and request.is_ajax:
+        try:
+            change_data = request.POST['change_data']
+        except:
+            return HttpResponse('Ошибка: 001',
+                                content_type='text/html; charset=utf-8')
+            # 001 - Ошибка запроса. Нет change_data
+
+        if change_data == 'userlist':
+            list = User.objects.order_by('username')
+            # количество элементов на странице:
+            paginator = Paginator(list, ITEMS_TO_PAGE)
             try:
-                change_data = request.POST['change_data']
+                page = request.POST['page_num']
             except:
-                return HttpResponse('Ошибка: 001',
-                                    content_type='text/html; charset=utf-8')
-                # 001 - Ошибка запроса. Нет change_data
+                page = 1
+            try:
+                list = paginator.page(page)
+            except PageNotAnInteger:
+                # If page is not an integer, deliver first page.
+                list = paginator.page(1)
+            except EmptyPage:
+                # If page is out of range (e.g. 9999),
+                #  deliver last page of results.
+                list = paginator.page(paginator.num_pages)
+            return render(request, 'admin_users/admin_userlist.html',
+                          {'list': list})
+            # Если надо отправить без http-заголовка
+            # html = loader.\
+            #     render_to_string('admin_users/admin_userlist.html',
+            #                      {'list': list}, request=request)
+            # data = {'errors': False, 'html': html}
+            # return JsonResponse(data)
 
-            if change_data == 'userlist':
-                list = User.objects.order_by('username')
-                # количество элементов на странице:
-                paginator = Paginator(list, ITEMS_TO_PAGE)
-                try:
-                    page = request.POST['page_num']
-                except:
-                    page = 1
-                try:
-                    list = paginator.page(page)
-                except PageNotAnInteger:
-                    # If page is not an integer, deliver first page.
-                    list = paginator.page(1)
-                except EmptyPage:
-                    # If page is out of range (e.g. 9999),
-                    #  deliver last page of results.
-                    list = paginator.page(paginator.num_pages)
-                return render(request, 'admin_users/admin_userlist.html',
-                              {'list': list})
-                # Если надо отправить без http-заголовка
-                # html = loader.\
-                #     render_to_string('admin_users/admin_userlist.html',
-                #                      {'list': list}, request=request)
-                # data = {'errors': False, 'html': html}
-                # return JsonResponse(data)
+        elif change_data == 'item_start_edit':
+            try:
+                item_id = request.POST['item_id']
+            except:
+                item_id = False
+            user = get_object_or_404(User, id=item_id)
+            return render(request, 'admin_users/admin_useredit.html',
+                          {'form': Edit_User_Form(instance=user),
+                           'formpassw': Edit_User_Passw(user),
+                           'id': item_id})
 
-            elif change_data == 'item_start_edit':
+        elif change_data == 'item_stop_edit':
+            try:
+                stop_item_id = request.POST['item_id']
+            except:
+                stop_item_id = False
+            if stop_item_id:
                 try:
-                    item_id = request.POST['item_id']
+                    item = User.objects.get(id=stop_item_id)
                 except:
-                    item_id = False
-                user = get_object_or_404(User, id=item_id)
-                return render(request, 'admin_users/admin_useredit.html',
-                              {'form': Edit_User_Form(instance=user),
-                               'formpassw': Edit_User_Passw(user),
-                               'id': item_id})
-
-            elif change_data == 'item_stop_edit':
-                try:
-                    stop_item_id = request.POST['item_id']
-                except:
-                    stop_item_id = False
-                if stop_item_id:
-                    try:
-                        item = User.objects.get(id=stop_item_id)
-                    except:
-                        item = False
-                    if item:
-                        return render(request,
-                                      'admin_users/admin_box_user_info.html',
-                                      {'item': item})
-                    else:
-                        return HttpResponse('<td colspan="10">Ошибка: Такого'
-                                            ' объекта нет в БД.</td>',
-                                            content_type='text/html;'
-                                                         ' charset=utf-8',
-                                            charset='utf-8')
+                    item = False
+                if item:
+                    return render(request,
+                                  'admin_users/admin_box_user_info.html',
+                                  {'item': item})
                 else:
-                    return HttpResponse('<td colspan="10">Ошибка: 004</td>',
+                    return HttpResponse('<td colspan="10">Ошибка: Такого'
+                                        ' объекта нет в БД.</td>',
                                         content_type='text/html;'
                                                      ' charset=utf-8',
                                         charset='utf-8')
-                    # 004 - Ошибка: передан некорректный id
-                    # (или не передан вовсе).
+            else:
+                return HttpResponse('<td colspan="10">Ошибка: 004</td>',
+                                    content_type='text/html;'
+                                                 ' charset=utf-8',
+                                    charset='utf-8')
+                # 004 - Ошибка: передан некорректный id
+                # (или не передан вовсе).
 
-            elif change_data == 'item_edit':
-                user = get_object_or_404(User, id=request.POST['id'])
-                form = Edit_User_Form(request.POST or None, instance=user)
-                if form.is_valid():
-                    form.save()
-                    return render(request,
-                                  'admin_users/admin_box_user_info.html',
-                                  {'item': user})
-
-                else:
-                    return render(request, 'admin_users/admin_useredit.html',
-                                  {'form': form, 'id': request.POST['id'],
-                                   'errors': form.errors,
-                                   'formpassw': Edit_User_Passw(user)})
-
-            elif change_data == 'item_delete':
-                try:
-                    del_id = request.POST['item_id']
-                except:
-                    del_id = False
-                if del_id:
-                    user = get_object_or_404(User, id=del_id)
-                    if user:
-                        user.delete()
-                        return HttpResponse('<td colspan="10">Данные успешно'
-                                            ' удалены.</td>',
-                                            content_type='text/html;'
-                                                         ' charset=utf-8',
-                                            charset='utf-8')
-                    else:
-                        return HttpResponse('<td colspan="10">Ошибка: Такого'
-                                            ' пользователя итак нет в'
-                                            ' БД.</td>',
-                                            content_type='text/html;'
-                                                         ' charset=utf-8',
-                                            charset='utf-8')
-                else:
-                    return HttpResponse('<td colspan="10">Ошибка: 004</td>',
-                                        content_type='text/html;'
-                                                     ' charset=utf-8',
-                                        charset='utf-8')
-                    # 004 - Ошибка: передан некорректный id
-                    # (или не передан вовсе).
-
-            elif change_data == 'item_edit_passw':
-                user = get_object_or_404(User, id=request.POST['id'])
-                form = Edit_User_Form(instance=user)
-                # formpassw = User.objects.get(id=request.POST['id'])
-                formpassw = get_object_or_404(User, id=request.POST['id'])
-                formpassw = Edit_User_Passw(formpassw, request.POST or None)
-                if formpassw.is_valid():
-                    formpassw.save()
-                    return render(request,
-                                  'admin_users/admin_box_user_info.html',
-                                  {'item': user})
-                else:
-                    return render(request, 'admin_users/admin_useredit.html',
-                                  {'form': form, 'id': request.POST['id'],
-                                   'errors_passw': formpassw.errors,
-                                   'formpassw': formpassw})
+        elif change_data == 'item_edit':
+            user = get_object_or_404(User, id=request.POST['id'])
+            form = Edit_User_Form(request.POST or None, instance=user)
+            if form.is_valid():
+                form.save()
+                return render(request,
+                              'admin_users/admin_box_user_info.html',
+                              {'item': user})
 
             else:
-                return HttpResponse('Ошибка: 002',
-                                    content_type='text/html; charset=utf-8')
-                # 002 - Ошибка запроса. change_data недопустимая
+                return render(request, 'admin_users/admin_useredit.html',
+                              {'form': form, 'id': request.POST['id'],
+                               'errors': form.errors,
+                               'formpassw': Edit_User_Passw(user)})
+
+        elif change_data == 'item_delete':
+            try:
+                del_id = request.POST['item_id']
+            except:
+                del_id = False
+            if del_id:
+                user = get_object_or_404(User, id=del_id)
+                if user:
+                    user.delete()
+                    return HttpResponse('<td colspan="10">Данные успешно'
+                                        ' удалены.</td>',
+                                        content_type='text/html;'
+                                                     ' charset=utf-8',
+                                        charset='utf-8')
+                else:
+                    return HttpResponse('<td colspan="10">Ошибка: Такого'
+                                        ' пользователя итак нет в'
+                                        ' БД.</td>',
+                                        content_type='text/html;'
+                                                     ' charset=utf-8',
+                                        charset='utf-8')
+            else:
+                return HttpResponse('<td colspan="10">Ошибка: 004</td>',
+                                    content_type='text/html;'
+                                                 ' charset=utf-8',
+                                    charset='utf-8')
+                # 004 - Ошибка: передан некорректный id
+                # (или не передан вовсе).
+
+        elif change_data == 'item_edit_passw':
+            user = get_object_or_404(User, id=request.POST['id'])
+            form = Edit_User_Form(instance=user)
+            # formpassw = User.objects.get(id=request.POST['id'])
+            formpassw = get_object_or_404(User, id=request.POST['id'])
+            formpassw = Edit_User_Passw(formpassw, request.POST or None)
+            if formpassw.is_valid():
+                formpassw.save()
+                return render(request,
+                              'admin_users/admin_box_user_info.html',
+                              {'item': user})
+            else:
+                return render(request, 'admin_users/admin_useredit.html',
+                              {'form': form, 'id': request.POST['id'],
+                               'errors_passw': formpassw.errors,
+                               'formpassw': formpassw})
+
         else:
-            return HttpResponse('Ошибка: 003',
+            return HttpResponse('Ошибка: 002',
                                 content_type='text/html; charset=utf-8')
-            # 003 - Ошибка: Не ajax или не POST
+            # 002 - Ошибка запроса. change_data недопустимая
+    else:
+        return HttpResponse('Ошибка: 003',
+                            content_type='text/html; charset=utf-8')
+        # 003 - Ошибка: Не ajax или не POST
